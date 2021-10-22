81833e8009af1607db23971bdcf494cac5f0ffd5
Hemanth kumar <ganeshhemanth9686@gmail.com>
2021-10-22 17:29:46 +0530

    updated code to limit results

--- src/index.ts
+++ src/index.ts
@@ -152,14 +152,30 @@ class Lsdb {
    * @param {WhereOptions} where Options which consist of mongo-like definition
    * @returns {Collection|undefined} Collection or undefined
    */
-  find(entity: string, { where }: { where: WhereOptions<Document> }): Collection | undefined {
+  find(entity: string, { where,limit }: { where: WhereOptions<Document>,limit?: number }): Collection | undefined {
     const dataset = this.collections[entity];
 
     const { valueToFilterBy, field, operator, options } = this.handleWhere(where);
 
+    if(limit){
+      const limiteddataset=dataset.filter((x: Query<GenericObject>) =>
+      OperatorOperations[operator as Operator](x[field], valueToFilterBy, options));
+      if(limiteddataset.length <= limit){
+        return limiteddataset;
+
+      }
+
+      else{
+        
+        return limiteddataset.slice(0,limit);
+      }
+    }
+
+    else{
     return dataset.filter((x: Query<GenericObject>) =>
       OperatorOperations[operator as Operator](x[field], valueToFilterBy, options),
     );
+    }
   }
 
   /**
